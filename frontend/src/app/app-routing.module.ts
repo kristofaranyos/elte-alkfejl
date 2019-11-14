52f97727eadc5d7ea2430769506139668971c3aa
Aranyos Kristof <hivua5@inf.elte.hu>
2019-11-14 17:36:04 +0100

    basic routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,40 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { IndexComponent } from './index/index.component';
+import { StockComponent } from './stock/stock.component';
+import { ItemComponent } from './item/item.component';
+import { WarehouseComponent } from './warehouse/warehouse.component';
+import { VendorComponent } from './vendor/vendor.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+	{
+    	path: '',
+    	component: IndexComponent
+	},
+	{
+    	path: 'stock',
+    	component: StockComponent
+	},
+	{
+   		path: 'item',
+    	component: ItemComponent
+	},
+	{
+    	path: 'warehouse',
+		component: WarehouseComponent
+	},
+	{
+    	path: 'vendor',
+		component: VendorComponent
+	},
+	{
+    	path: 'user',
+		component: UserComponent
+	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,16 +5,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatToolbarModule, MatIconModule, MatButtonModule } from '@angular/material';
+import { ItemComponent } from './item/item.component';
+import { StockComponent } from './stock/stock.component';
+import { WarehouseComponent } from './warehouse/warehouse.component';
+import { IndexComponent } from './index/index.component';
+import { VendorComponent } from './vendor/vendor.component';
+import { UserComponent } from './user/user.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NoopAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ItemComponent,
+        StockComponent,
+        WarehouseComponent,
+        IndexComponent,
+        VendorComponent,
+        UserComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NoopAnimationsModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
+
 export class AppModule { }
